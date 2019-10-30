cd30151476b7d300b0da2148234a9a074a4dbb7d
Adam Lee <adam.lee@activatedresearch.com>
2019-10-30 13:21:35 -0500

    add flags for code or powershell only

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,13 +16,5 @@ module.exports = {
   },
   rules: {
     "no-console": "off",
-    "no-restricted-syntax": [
-      "error",
-      {
-          "selector": "CallExpression[callee.object.name='console'][callee.property.name!=/^(log|warn|error|info|trace)$/]",
-          "message": "Unexpected property on console object was called"
-      }
-  ]
-}
   },
 };
--- index.js
+++ index.js
@@ -13,25 +13,35 @@ const settings = userSettings.file('.codenow');
 program
   .option('-d, --dir [directory]', 'set repositories directory')
   .option('-l, --list', 'list repository names')
+  .option('-c, --code', 'open dev environment only')
+  .option('-p, --powershell', 'open powershell only')
   .parse(process.argv);
 
+function getDirSetting() {
+  return settings.get('dir');
+}
+
+function setDirSetting(dir) {
+  settings.set('dir', dir);
+}
+
 function handleDir(dir) {
-  if (program.dir === true) {
-    if (settings.get('dir')) {
+  if (dir === true) {
+    if (getDirSetting()) {
       console.log(chalk.green(`currently using ${settings.get('dir')}`));
     } else {
       console.log(chalk.red('dir is not set'));
     }
-  } else if (fs.existsSync(program.dir)) {
-    settings.set('dir', program.dir);
-    console.log(chalk.green(`dir set to ${program.dir}`));
+  } else if (fs.existsSync(dir)) {
+    setDirSetting(dir);
+    console.log(chalk.green(`dir set to ${dir}`));
   } else {
-    console.log(chalk.red(`${program.dir} does not exist`));
+    console.log(chalk.red(`${dir} does not exist`));
   }
 }
 
 function handleList() {
-  const dir = settings.get('dir')
+  const dir = getDirSetting();
   if (!dir) {
     console.log(chalk.red('dir is not set'));
     return;
@@ -43,7 +53,7 @@ function handleList() {
   fs.readdir(dir, (err, items) => {
     items.forEach((item) => {
       console.log(chalk.green(item));
-    })
+    });
   });
 }
 
@@ -55,8 +65,12 @@ if (program.dir) {
   const repository = path.join(settings.get('dir'), program.args[0]);
   if (fs.existsSync(repository)) {
     console.log(chalk.green(`found ${program.args[0]} in ${repository}`));
-    exec(`powershell ${path.join(__dirname, 'start-powershell.ps1')} "${repository}"`);
-    exec(`code ${repository}`);
+    if (!program.code) {
+      exec(`powershell ${path.join(__dirname, 'start-powershell.ps1')} "${repository}"`);
+    }
+    if (!program.powershell) {
+      exec(`code ${repository}`);
+    }
   } else {
     console.log(chalk.red(`could not access ${repository}`));
   }
